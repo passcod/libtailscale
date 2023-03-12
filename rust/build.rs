21680f1010f9d64bd7d850f2fc46ad6d7fddbe62
Jan-Erik Rediger <janerik@fnordig.de>
2023-03-12 14:28:51 +0100

    Specify output correctly

--- rust/build.rs
+++ rust/build.rs
@@ -1,10 +1,16 @@
+use std::env;
 use std::process::Command;
 
 fn main() {
+    let outdir = env::var("OUT_DIR").unwrap();
+    let out = format!("{outdir}/libtailscale.a");
     let status = Command::new("go")
-        .args(["build", "-buildmode=c-archive", ".."])
+        .args(["build", "-buildmode=c-archive", "-o", &out, ".."])
         .status()
         .expect("can't build go library");
 
     assert!(status.success(), "failed to build go library");
+
+    println!("cargo:rustc-link-search={outdir}");
+    println!("cargo:rustc-link-lib=static=tailscale");
 }

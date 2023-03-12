66fc19bfb70c0e0f87b7e6874108b0d9478a17dc
Jan-Erik Rediger <janerik@fnordig.de>
2023-03-12 16:00:12 +0100

    make it packagable

--- rust/build.rs
+++ rust/build.rs
@@ -2,10 +2,11 @@ use std::env;
 use std::process::Command;
 
 fn main() {
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let outdir = env::var("OUT_DIR").unwrap();
     let out = format!("{outdir}/libtailscale.a");
     let status = Command::new("go")
-        .args(["build", "-buildmode=c-archive", "-o", &out, ".."])
+        .args(["build", "-buildmode=c-archive", "-o", &out, &manifest_dir])
         .status()
         .expect("can't build go library");
 

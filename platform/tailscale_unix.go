99cbc261a962fc640ff8753ba57b23e86895b22d
Félix Saparelli <felix@passcod.name>
2024-02-10 23:00:05 +1300

    Same signatures for platform on win vs unix

--- platform/tailscale_unix.go
+++ platform/tailscale_unix.go
@@ -4,7 +4,6 @@
 package platform
 
 //#include "errno.h"
-//#include "../socketpair_handler.h"
 import "C"
 
 import (
@@ -15,19 +14,19 @@ func GetSocketPair() ([2]int, error) {
 	return syscall.Socketpair(syscall.AF_LOCAL, syscall.SOCK_STREAM, 0)
 }
 
-func CloseSocket(fd interface{}) (err error) {
-	return syscall.Close(fd.(int))
+func CloseSocket(fd int) (err error) {
+	return syscall.Close(fd)
 }
 
-func ReadSocket(fd interface{}, buf *[256]byte) {
-	syscall.Read(fd.(int), (*buf)[:])
+func ReadSocket(fd int, buf *[256]byte) {
+	syscall.Read(fd, (*buf)[:])
 }
 
-func SendMessage(fd interface{}, p []byte, connFd int, to syscall.Sockaddr, flags int) (err error) {
-	rights := syscall.UnixRights(int(connFd))
-	return syscall.Sendmsg(fd.(int), p, rights, to, flags)
+func SendMessage(fd int, p []byte, connFd int, to syscall.Sockaddr, flags int) (err error) {
+	rights := syscall.UnixRights(connFd)
+	return syscall.Sendmsg(fd, p, rights, to, flags)
 }
 
-func Shutdown(fd interface{}, how int) (err error) {
-	return syscall.Shutdown(fd.(int), how)
+func Shutdown(fd int, how int) (err error) {
+	return syscall.Shutdown(fd, how)
 }

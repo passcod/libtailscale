eb80e8760d4fbc99cd9de09f5d4e629b204de72c
Félix Saparelli <felix@passcod.name>
2024-02-10 23:11:56 +1300

    try to appease go
    me 0:1 inscrutable go errors

--- platform/tailscale_windows.go
+++ platform/tailscale_windows.go
@@ -9,43 +9,43 @@ import (
 
 // simulate socketpair by creating two connected IPv4 sockets at a random port
 func GetSocketPair() ([2]int, error) {
-	listen_sock, err = syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+	listen_sock, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
 	if err != nil {
 		return nil, err
 	}
 
-	err = syscall.Bind(listen_sock, &syscall.SockaddrInet4{Port: 0, Addr: [4]byte{127, 0, 0, 1}})
+	err := syscall.Bind(listen_sock, &syscall.SockaddrInet4{Port: 0, Addr: [4]byte{127, 0, 0, 1}})
 	if err != nil {
 		return nil, err
 	}
 
-	err = syscall.Listen(listen_sock, 1)
+	err := syscall.Listen(listen_sock, 1)
 	if err != nil {
 		return nil, err
 	}
 
 	// get the effective port number
-	sockaddr, err = syscall.GetSockName(listen_sock)
+	sockaddr, err := syscall.GetSockName(listen_sock)
 	if err != nil {
 		return nil, err
 	}
 
-	client_sock, err = syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+	client_sock, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
 	if err != nil {
 		return nil, err
 	}
 
-	err = syscall.Connect(client_sock, sockaddr)
+	err := syscall.Connect(client_sock, sockaddr)
 	if err != nil {
 		return nil, err
 	}
 
-	send_sock, _, err = syscall.Accept(listen_sock)
+	send_sock, _, err := syscall.Accept(listen_sock)
 	if err != nil {
 		return nil, err
 	}
 
-	err = syscall.Close(listen_sock)
+	err := syscall.Close(listen_sock)
 	if err != nil {
 		return nil, err
 	}
